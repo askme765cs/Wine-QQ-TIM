c52b719c83a4cbd5cd5dc3132e7de88c9d603beb
askme765cs <askme765cs@yahoo.com>
2017-05-20 23:54:53 +0800

    Wine-QQ 基于QQ 8.9.2 变更：1）添加安装与卸载脚本2）添加了基于winetricks的管理工具

---
+++ Wine-QQ8.9.2/Install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+mkdir /opt/QQ8.9.2
+cp -R qq /opt/QQ8.9.2/qq
+cp  qq.sh /opt/QQ8.9.2/qq.sh
+cp  Wine-QQ-Tool.sh /opt/QQ8.9.2/Wine-QQ-Tool.sh
+cp  icon/QQ.png /opt/QQ8.9.2/qq.png
+cp  icon/Wine-QQ-Tool.png  /opt/QQ8.9.2/Wine-QQ-Tool.png
+cp QQ.desktop /usr/share/applications/QQ.desktop
+cp Wine-QQ-Tool.desktop /usr/share/applications/Wine-QQ-Tool.desktop
+chmod -R 
+chmod 744 /usr/share/applications/QQ.desktop /usr/share/applications/Wine-QQ-Tool.desktop
+sudo chown "$USER":users -R /opt/QQ8.9.2
+chmod -R 755 /opt/QQ8.9.2
+echo "安装完成" 
